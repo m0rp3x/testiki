6f3eadd19f6ea0fa598cf5d3b885f7330f9cbede
m0rp3x <yurash612@gmail.com>
2022-05-05 14:22:51 +0300

    тесты доделаны

---
+++ calcul/calculator.py
@@ -0,0 +1,18 @@
+import pytest
+
+class Calculator():
+
+    def summary(self,x , y):
+     return x+y
+
+    def ne_summary(self,x,y):
+        return x-y
+
+    def multiply(self,x , y):
+        return x*y
+
+    def ne_muptiply_s_ostatkom(self, x,y):
+        return x/y
+
+    def ne_multiply_bez_ostatka(self,x,y):
+        return x//y
--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 import pytest
-from app.calculator import Сalculator
+from calcul.calculator import Calculator
 
-cal = Сalculator()
+cal = Calculator()
 
-def test_passing():
+def testik_passing():
     assert cal.summary(4, 5) == 9
-    assert cal.otnimanie(3, 2) == 1
-    assert cal.ymnogenie(3, 2) == 6
-    assert cal.delenie_ostatok(3, 2) == 1.5
-    assert cal.delenie_bez_ostatka(3, 2) == 1
-
-
+def tested_passing():
+    assert cal.ne_summary(3, 2) == 1
+def testyd_passing():
+    assert cal.multiply(3, 2) == 6
+def testob_passing():
+    assert cal.ne_muptiply_s_ostatkom(3,   1) == 3
+def test_passobie():
+    assert cal.ne_multiply_bez_ostatka(3, 2) == 1
